d018a280f473ef0e30c7bdc5babd36f90d1a56c0
Nikolai Karnaukhov <kolya-0310@mail.ru>
2021-05-27 17:52:11 +1000

    B2 All tests 27/05/2021

--- src/main/java/com/javacourse/stack/ArrayStack.java
+++ src/main/java/com/javacourse/stack/ArrayStack.java
@@ -5,21 +5,70 @@ package com.javacourse.stack;
  */
 public class ArrayStack<ItemTypeT> implements Stack<ItemTypeT> {
 
+	private int arraySize = 0;
+	private Object[] stackArray;
+	private int head;
+	private int difference = 10;
+
+	public ArrayStack(){
+		stackArray = new Object[arraySize];
+		head = -1;
+	}
 	@Override
 	public void push(ItemTypeT item) {
-		// TODO: замените тело метода на полноценную реализацию
-		throw new RuntimeException("Method not implemented");
+		if(head == -1){
+			arraySize++;
+			stackArray = new Object[arraySize];
+		}
+		if(head !=arraySize -1){
+			head++;
+			stackArray[head] = item;
+		}
+		else{
+			head++;
+			arraySize = arraySize+difference;
+			Object[] newArray = new Object[arraySize];
+			System.arraycopy(stackArray,0,newArray,0,arraySize - difference);
+			stackArray = null;
+			newArray[head] = item;
+			stackArray = newArray;
+		}
 	}
 
 	@Override
 	public ItemTypeT pop() {
-		// TODO: замените тело метода на полноценную реализацию
-		throw new RuntimeException("Method not implemented");
+		if(head == -1){
+			throw  new RuntimeException("Empty Stack Exception");
+		}
+		if(head ==0){
+			arraySize--;
+			head--;
+			Object temp = stackArray[0];
+			stackArray = null;
+			return (ItemTypeT) temp;
+		}
+		else{
+			Object temp = stackArray[head];
+			head--;
+
+			if(arraySize % difference == 0){
+				arraySize--;
+				Object[] newArray = new Object[arraySize];
+				System.arraycopy(stackArray,0,newArray, 0,arraySize);
+				stackArray = null;
+				stackArray = newArray;
+				return (ItemTypeT) temp;
+			}
+			else{
+				arraySize--;
+				return  (ItemTypeT) temp;
+			}
+		}
 	}
 
 	@Override
 	public ItemTypeT peek() {
-		// TODO: замените тело метода на полноценную реализацию
-		throw new RuntimeException("Method not implemented");
+		if(head != -1) {return (ItemTypeT) stackArray[head];}
+		else throw new RuntimeException("Empty Stack Exception");
 	}
 }
